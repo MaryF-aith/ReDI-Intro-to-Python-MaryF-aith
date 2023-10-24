266e75d05dcc54e5594a14a064686d6da1e561d7
neslihankeskin <neslihankeskin91@gmail.com>
2023-10-24 17:37:53 +0200

    update on VSC

--- 2-Python-Tools/Homework1-VSC-solution.py
+++ 2-Python-Tools/Homework1-VSC-solution.py
@@ -1,6 +1,5 @@
 #This is my first code
 print("Hello Intro to Computer Science")
-
 #Task 1: Write a print statement which prints "Hello, world"
 print("Hello, world)")
 #Task 2: Assing your name to a variable which is called myname, then print the variable
